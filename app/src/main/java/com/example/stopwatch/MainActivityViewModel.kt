1d1e82e8f7a67066d2ce196a71cd6170f47cc690
Saurabh K. Sonar <saurabh.sonar120@gmail.com>
2022-03-24 12:54:13 +0530

    Time class

--- app/src/main/java/com/example/stopwatch/MainActivityViewModel.kt
+++ app/src/main/java/com/example/stopwatch/MainActivityViewModel.kt
@@ -2,8 +2,18 @@ package com.example.stopwatch
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.util.*
 
 class MainActivityViewModel: ViewModel() {
     val time= MutableLiveData<Time>()
     val lapTime= MutableLiveData<Time>()
+    val timer=Timer()
+    val timerTask=object :TimerTask(){
+        override fun run() {
+            time.value?.increment()
+
+        }
+    }
+    fun startTimer(){
+    }
 }
--- app/src/main/java/com/example/stopwatch/Time.kt
+++ app/src/main/java/com/example/stopwatch/Time.kt
@@ -1,3 +1,19 @@
 package com.example.stopwatch
 
-data class Time(val minutes:Int,val seconds:Int)
+data class Time(var minutes:Int=0,var seconds:Int=0){
+    fun increment(){
+        seconds++
+        minutes+=seconds%60
+    }
+    override fun toString(): String {
+        var minutesString=minutes.toString()
+        var secondsString=seconds.toString()
+        if(minutesString.length<2)  minutesString="0"+minutesString)
+        if(secondsString.length<2)  secondsString="0"+secondsString
+        return "$minutesString:$secondsString"
+    }
+    fun reset(){
+        minutes=0
+        seconds=0
+    }
+}
